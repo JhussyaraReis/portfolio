1655a3cfe2c0800c7d61c7767af9ab9e91ba5fbf
JhussyaraReis <147738753+JhussyaraReis@users.noreply.github.com>
2024-02-15 17:08:54 -0300

    bug fixed

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,14 +1,24 @@
 const acordeonTrigger = document.querySelectorAll(".acordeon .trigger");
 
+const abrir = (e) => {
+  const acordeon = e.parentElement;
+  const isOpen = acordeon.classList.contains("open");
+
+  if (isOpen) {
+    acordeon.classList.remove("open");
+  } else {
+    acordeon.classList.add("open");
+  }
+};
+
 acordeonTrigger.forEach((trigger) => {
   trigger.addEventListener("click", (e) => {
-    const acordeon = e.target.parentElement;
-    const isOpen = acordeon.classList.contains("open");
-
-    if (isOpen) {
-      acordeon.classList.remove("open");
-    } else {
-      acordeon.classList.add("open");
-    }
+    abrir(e.target);
+  });
+  trigger.children[0].addEventListener("click", (e) => {
+    abrir(e.target.parentElement);
+  });
+  trigger.children[1].addEventListener("click", (e) => {
+    abrir(e.target.parentElement);
   });
 });
